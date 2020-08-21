bf8b57e52defef537e3e39708fad7ad3c190ed86
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    Phoenix: Create a Tethering RRO package.  * Tethering package is APEX and we need to create an    RRO to overwrite values of:
       config_tether_bluetooth_regexs
       config_tether_dhcp_range
       config_tether_upstream_types
       config_tether_usb_regexs
       config_tether_wifi_regexs
       config_tether_wifi_p2p_regexs
    
    Change-Id: I4eca6ba73bc0f908531fd89941ca5e1618d4b015

--- device.mk
+++ device.mk
@@ -434,6 +434,7 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     libwpa_client \
     WifiOverlay \
+    TetheringConfigOverlay \
     wpa_supplicant \
     wpa_supplicant.conf \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
